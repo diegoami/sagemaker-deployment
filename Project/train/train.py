162227c3bc86eef97f10e19491edea273f54f6c7
Diego Amicabile <diego.amicabile@gmail.com>
2020-07-16 18:31:59 +0200

    Sagemaker project

--- Project/train/train.py
+++ Project/train/train.py
@@ -56,20 +56,26 @@ def _get_train_data_loader(batch_size, training_dir):
 
 
 def train(model, train_loader, epochs, optimizer, loss_fn, device):
-    """
-    This is the training method that is called by the PyTorch training script. The parameters
-    passed are as follows:
-    model        - The PyTorch model that we wish to train.
-    train_loader - The PyTorch DataLoader that should be used during training.
-    epochs       - The total number of epochs to train for.
-    optimizer    - The optimizer to use during training.
-    loss_fn      - The loss function used for training.
-    device       - Where the model and data should be loaded (gpu or cpu).
-    """
-    
-    # TODO: Paste the train() method developed in the notebook here.
+    model.to(device)
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        for batch in train_loader:
+            batch_X, batch_y = batch
 
-    pass
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+            prediction = model(batch_X)
+            # calculate the loss
+            loss = loss_fn(prediction, batch_y)
+            # zero gradients
+            optimizer.zero_grad()
+            # perform backprop and update weights
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.data.item()
+        print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))s
 
 
 if __name__ == '__main__':
