b5260cba4e2deb418457ecd40923fa553755641f
Diego Amicabile <diego.amicabile@gmail.com>
2020-07-22 02:05:19 +0200

    feat: Implement Sentiment Analysis with LSTM

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -57,6 +57,7 @@ def output_fn(prediction_output, accept):
     print('Serializing the generated output.')
     return str(prediction_output)
 
+
 def predict_fn(input_data, model):
     print('Inferring sentiment of input data.')
 
@@ -65,13 +66,12 @@ def predict_fn(input_data, model):
     if model.word_dict is None:
         raise Exception('Model has not been loaded properly, no word_dict.')
     
-    # TODO: Process input_data so that it is ready to be sent to our model.
+    # Process input_data so that it is ready to be sent to our model.
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
 
-    data_X = convert_and_pad(word_dict, review_to_words(input_data))
-    data_len = len(data_X)
+    data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data))
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -84,9 +84,11 @@ def predict_fn(input_data, model):
     # Make sure to put the model into evaluation mode
     model.eval()
 
-    # TODO: Compute the result of applying the model to the input data. The variable `result` should
+    # Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = model(data)
+    tensor = model(data)
+    float_result = tensor.detach().cpu().numpy() 
+    result = np.rint(float_result).astype(int)  
 
     return result
